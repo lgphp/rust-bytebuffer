10473aebc8399912e13fe98e71b0f71a30020b25
Takeo Miyamoto <miyamofigo@gmail.com>
2015-10-24 18:33:28 +0900

    test function has been implemented.

--- src/lib.rs
+++ src/lib.rs
@@ -18,18 +18,18 @@ pub struct ByteBuffer {
   byte_order: BO, 
 }
 
-#[derive(Clone)]
-enum BO {
+#[derive(Clone,PartialEq,Eq)]
+pub enum BO {
   BigEndian,
   LittleEndian,
 }
 
 impl ByteBuffer {
-  fn allocate(capacity: usize) -> Self {
+  pub fn allocate(capacity: usize) -> Self {
     ByteBuffer {
       buf: Rc::new(RawVec::with_capacity(capacity)),
       pos: 0,
-      limit: capacity,
+      limit: capacity - 1,
       mark: None,
       offset: 0,
       byte_order: BO::BigEndian,
@@ -37,29 +37,29 @@ impl ByteBuffer {
   }
 }
 
-trait Buffer {
+pub trait Buffer {
   fn cap(&self) -> usize;
   fn get_pos(&self) -> usize;
-  fn set_pos(&mut self, newpos: usize) -> Result<&Self, &str>; 
+  fn set_pos(&mut self, newpos: usize) -> Result<&mut Self, &str>; 
   fn get_lim(&self) -> usize;
-  fn set_lim(&mut self, newlim: usize) -> Result<&Self, &str>; 
-  fn mark(&mut self) -> &Self;
-  fn reset(&mut self) -> Result<&Self, &str>;
-  fn clear(&mut self) -> &Self;
-  fn flip(&mut self) -> &Self;
-  fn rewind(&mut self) -> &Self;
+  fn set_lim(&mut self, newlim: usize) -> Result<&mut Self, &str>; 
+  fn mark(&mut self) -> &mut Self;
+  fn reset(&mut self) -> Result<&mut Self, &str>;
+  fn clear(&mut self) -> &mut Self;
+  fn flip(&mut self) -> &mut Self;
+  fn rewind(&mut self) -> &mut Self;
   fn remaining(&self) -> usize;
   fn has_remaining(&self) -> bool; 
   fn next_index(&mut self) -> Result<usize, &str>; 
-  fn get_order<'a>(&'a self) -> &'a BO;
-  fn set_order(&mut self, bo: BO) -> &Self;
+  fn get_order(&self) -> BO;
+  fn set_order(&mut self, bo: BO) -> &mut Self;
 }
 
 impl Buffer for ByteBuffer {
   fn cap(&self) -> usize { self.buf.cap() - self.offset }
 
   fn get_pos(&self) -> usize { self.pos - self.offset }
-  fn set_pos(&mut self, newpos: usize) -> Result<&Self, &str>  {
+  fn set_pos(&mut self, newpos: usize) -> Result<&mut Self, &str>  {
     if newpos > self.limit { 
       return Err("The new position is beyond the limit!!");
     }     
@@ -73,8 +73,8 @@ impl Buffer for ByteBuffer {
   } 
 
   fn get_lim(&self) -> usize { self.limit - self.offset }
-  fn set_lim(&mut self, newlim: usize) -> Result<&Self, &str>  {
-    if newlim > self.cap() { 
+  fn set_lim(&mut self, newlim: usize) -> Result<&mut Self, &str>  {
+    if newlim >= self.cap() { 
       return Err("You cannot set the limit beyond the buffer capacity!!");
     }     
 
@@ -87,10 +87,10 @@ impl Buffer for ByteBuffer {
     Ok(self)
   }
   
-  fn mark(&mut self) -> &Self {
+  fn mark(&mut self) -> &mut Self {
     self.mark = Some(self.pos); self
   }
-  fn reset(&mut self) -> Result<&Self, &str> {
+  fn reset(&mut self) -> Result<&mut Self, &str> {
     match self.mark {
       None => return Err("Mark has not been set yet."),
       Some(mark) => self.pos = mark,
@@ -99,28 +99,28 @@ impl Buffer for ByteBuffer {
   }
 
   // This method does not actually erase the data in the buffer. 
-  fn clear(&mut self) -> &Self {
+  fn clear(&mut self) -> &mut Self {
     self.set_pos(0);
     let cap = self.cap();
-    self.set_lim(cap);
+    self.set_lim(cap - 1);
     self.mark.take();
     self  
   }
 
-  fn flip(&mut self) -> &Self {
+  fn flip(&mut self) -> &mut Self {
     self.limit = self.get_pos();
     self.set_pos(0);
     self.mark.take();
     self
   }
 
-  fn rewind(&mut self) -> &Self {
+  fn rewind(&mut self) -> &mut Self {
     self.set_pos(0);
     self.mark.take();
     self
   }
 
-  fn remaining(&self) -> usize { self.get_lim() - self.get_pos() }
+  fn remaining(&self) -> usize { self.get_lim() - self.get_pos() + 1 }
   fn has_remaining(&self) -> bool { self.get_pos() < self.get_lim() }
 
   // You have to notice that next_index function returns the current position!! 
@@ -133,15 +133,15 @@ impl Buffer for ByteBuffer {
     Ok(pos)
   }   
 
-  fn get_order<'a>(&'a self) -> &'a BO { &self.byte_order } 
-  fn set_order(&mut self, bo: BO) -> &Self { 
+  fn get_order(&self) -> BO { self.byte_order.clone() } 
+  fn set_order(&mut self, bo: BO) -> &mut Self { 
     self.byte_order = bo; self
   }
    
 }
 
 impl ByteBuffer {
-  fn wrap(bytes: &[u8]) -> ByteBuffer {
+  pub fn wrap(bytes: &[u8]) -> ByteBuffer {
     let bb = ByteBuffer::allocate(bytes.len());
     let mut cnt = 0;  
     for b in bytes.into_iter() { 
@@ -154,7 +154,7 @@ impl ByteBuffer {
     bb
   }
 
-  fn put(&mut self, byte: u8) -> &Self {
+  pub fn put(&mut self, byte: u8) -> &mut Self {
     unsafe {
       std::ptr::write((*self.buf).ptr().offset(
                         match self.next_index() {
@@ -166,7 +166,7 @@ impl ByteBuffer {
     self
   }
 
-  fn put_bytes(&mut self, bytes: &[u8]) -> Result<&Self, &str> {
+  pub fn put_bytes(&mut self, bytes: &[u8]) -> Result<&mut Self, &str> {
     if bytes.len() > self.remaining() {
       return Err("You can't write bytes beyond the limit!!");
     }
@@ -174,7 +174,7 @@ impl ByteBuffer {
     Ok(self)
   }
 
-  fn get(&mut self) -> u8 {
+  pub fn get(&mut self) -> u8 {
     unsafe {
       std::ptr::read((*self.buf).ptr().offset(
                        match self.next_index() {
@@ -184,7 +184,7 @@ impl ByteBuffer {
     }
   }
   
-  fn slice(&self) -> ByteBuffer {
+  pub fn slice(&self) -> ByteBuffer {
     let pos = self.get_pos();
     let lim = self.get_lim();
     assert!(pos <= lim);  
@@ -200,7 +200,7 @@ impl ByteBuffer {
     }
   } 
   
-  fn get_bytes(&mut self, len: usize) -> Result<&[u8], &str> {
+  pub fn get_bytes(&mut self, len: usize) -> Result<&[u8], &str> {
     if len > self.remaining() {
       return Err("The remaining buffer size is shorter than one you specified!");
     }
@@ -208,6 +208,8 @@ impl ByteBuffer {
     let raw_result = unsafe {
       (*self.buf).ptr().offset(self.pos as isize)
     };
+
+    for _ in 0..len { self.next_index(); }
     Ok(unsafe { std::slice::from_raw_parts(raw_result, len) })
   }
 }
@@ -215,22 +217,22 @@ impl ByteBuffer {
 macro_rules! define_trait {
   ( get, $($x:tt),* ) => ( 
     interpolate_idents! {
-      trait Getter {
+      pub trait Getter {
         $( fn [get_ $x] (&mut self) -> Option<$x>; )*
       }
     }
   );
   ( put, $($x:tt),* ) => ( 
     interpolate_idents! {
-      trait Putter {
-        $( fn [put_ $x] (&mut self, n: $x) -> Result<&Self, &str>; )*
+      pub trait Putter {
+        $( fn [put_ $x] (&mut self, n: $x) -> Result<&mut Self, &str>; )*
       }
     }
   );
-  ( slice, $($x:tt),* ) => (
+  ( vector, $($x:tt),* ) => (
     interpolate_idents! {
-      trait AsSlice {
-        $( fn [as_ $x _slice] (&mut self) -> Option<Vec<$x>>; )*
+      pub trait AsVec {
+        $( fn [as_ $x _vec] (&mut self) -> Option<Vec<$x>>; )*
       }
     }
   );  
@@ -240,7 +242,7 @@ macro_rules! define_traits {
   ( $($x:ident),* ) => {
     define_trait!(get, $($x),*); 
     define_trait!(put, $($x),*); 
-    define_trait!(slice, $($x),*); 
+    define_trait!(vector, $($x),*); 
   }
 }
 
@@ -268,9 +270,13 @@ macro_rules! implement_trait {
   ( put, $($x:tt),* ) => (
     interpolate_idents! {
       impl Putter for ByteBuffer {
-        $( fn [put_ $x] (&mut self, n: $x) -> Result<&Self, &str> {
+        $( fn [put_ $x] (&mut self, n: $x) -> Result<&mut Self, &str> {
              let len = std::mem::size_of::<$x>();
-             let mut vec = std::vec::Vec::with_capacity(len);
+             let mut vec = Vec::<u8>::with_capacity(len);
+
+             // oops!
+             for _ in 0..len { vec.push(0); } 
+
              let buf = vec.as_mut_slice();
              match self.byte_order {
                BO::LittleEndian => LittleEndian::[write_ $x](buf, n),
@@ -281,10 +287,10 @@ macro_rules! implement_trait {
       }
     }
   );
-  ( slice, $($x:tt),* ) => (
+  ( vector, $($x:tt),* ) => (
     interpolate_idents! {
-      impl AsSlice for ByteBuffer {
-        $( fn [as_ $x _slice] (&mut self) -> Option<Vec<$x>> { 
+      impl AsVec for ByteBuffer {
+        $( fn [as_ $x _vec] (&mut self) -> Option<Vec<$x>> { 
              let type_size = std::mem::size_of::<$x>();
              let len = match self.remaining() / type_size { 
                          0 => return None,
@@ -300,7 +306,9 @@ macro_rules! implement_trait {
                 std::slice::from_raw_parts(bytes, len)
                   .iter()
                   .map(|&num| { 
-                        let mut vec = Vec::with_capacity(len);
+                        let mut vec = Vec::<u8>::with_capacity(type_size);
+                        // oh, Gee!!
+                        for _ in 0..type_size { vec.push(0); }
                         let buf = vec.as_mut_slice();
                         match self.byte_order {
                           BO::LittleEndian => 
@@ -322,9 +330,161 @@ macro_rules! implement_traits {
   ( $($x:tt),* ) => { 
     implement_trait!(get, $($x),*); 
     implement_trait!(put, $($x),*); 
-    implement_trait!(slice, $($x),*); 
+    implement_trait!(vector, $($x),*); 
   }
 }
 
 implement_traits!(u16, u32, u64, i16, i32, i64, f32, f64);
 
+#[test]
+fn test_buffer() {
+  let mut buf = ByteBuffer::allocate(4);
+  assert!( buf.cap() == 4 );
+  assert!( buf.get_pos() == 0 );    
+  assert!( buf.set_pos(1).unwrap()
+              .get_pos() 
+            == 1 );   
+  assert!( buf.get_lim() == 3 );
+  assert!( buf.set_lim(2).unwrap()
+              .get_lim() 
+            == 2 );
+  assert!( buf.set_lim(10).is_err() );
+  buf.mark();
+  assert!( buf.mark == Some(1) );
+  assert!( buf.set_pos(2).unwrap()
+              .reset().unwrap()
+              .get_pos() 
+            == 1);
+  assert!( buf.clear().get_pos() == 0 
+            && buf.mark.is_none() 
+            && buf.get_lim() == 3 );
+  assert!( buf.set_pos(2).unwrap()
+              .mark()
+              .flip()
+              .get_lim() == 2
+            && buf.mark.is_none()
+            && buf.get_pos() == 0 );
+  assert!( buf.rewind()
+              .get_pos() == 0
+            && buf.mark.is_none() ); 
+  assert!( buf.remaining() == 3 );
+  assert!( buf.has_remaining() );
+  assert!( buf.next_index()
+              .unwrap() == 0
+            && buf.has_remaining()
+            && buf.remaining() == 2 
+            && buf.get_pos() == 1 );
+  assert!( buf.next_index()
+              .unwrap() == 1
+            && !buf.has_remaining() 
+            && buf.remaining() == 1 
+            && buf.get_pos() == 2
+            && buf.get_lim() == 2 );
+  assert!( buf.next_index().is_err() );
+  assert!( buf.get_order() == BO::BigEndian );
+  assert!( buf.set_order(BO::LittleEndian)
+              .get_order() == BO::LittleEndian );
+}
+
+#[test]
+fn test_bytebuffer() {
+  let rawbuf = [3u8; 4];   
+  let mut bb = ByteBuffer::wrap(&rawbuf);  
+  assert!( bb.get() == 3 );
+  assert!( bb.get() == 3 );
+  assert!( bb.get() == 3 );
+  assert!( bb.get() == 3 );
+  assert!( bb.get() == 3 );
+  assert!( bb.get_pos() == bb.get_lim() );
+  assert!( bb.put(4)
+             .get() == 4 ); 
+  assert!( bb.get_pos() == bb.get_lim() );
+  assert!( bb.put(5)
+             .get() == 5 );
+  let newbyte = [7u8; 4];
+  assert!( bb.put_bytes(&newbyte)
+             .is_err() );
+
+  bb.clear();
+  assert!( bb.put_bytes(&newbyte)
+             .is_ok() ); 
+  assert!( bb.get_pos() == bb.get_lim() );
+  bb.clear();
+  assert!( bb.get() == 7 );
+  assert!( bb.get() == 7 );
+  assert!( bb.get() == 7 );
+  assert!( bb.get() == 7 );
+  bb.clear();
+  let mut bbslice = bb.slice();
+  bbslice.put(1);
+  assert!( bb.get() == 1 );
+  bbslice.put(1);
+  assert!( bb.get() == 1 );
+  bbslice.put(1);
+  bbslice.put(1);
+  bb.clear();
+  for i in bb.get_bytes(4).unwrap().iter() {
+    assert!( *i == 1 );
+  }
+  assert!( bb.get_bytes(10).is_err() ); 
+}
+
+#[test]
+fn test_get_and_put() {
+  let mut bb = ByteBuffer::allocate(16);
+  bb.put_u16(10).unwrap().clear();
+  assert!( bb.get_u16().unwrap() == 10 );
+  bb.clear();
+  bb.put_u32(70).unwrap().clear();
+  assert!( bb.get_u32().unwrap() == 70 );
+  bb.clear();
+  bb.put_u64(7000).unwrap().clear();
+  assert!( bb.get_u64().unwrap() == 7000 );
+  bb.clear();
+  bb.put_i16(999).unwrap().clear();
+  assert!( bb.get_i16().unwrap() == 999 );
+  bb.clear();
+  bb.put_i32(77).unwrap().clear();
+  assert!( bb.get_i32().unwrap() == 77 );
+  bb.clear();
+  bb.put_i64(66666).unwrap().clear();
+  assert!( bb.get_i64().unwrap() == 66666 );
+  bb.clear();
+  bb.put_f32(77.777).unwrap().clear();
+  assert!( bb.get_f32().unwrap() == 77.777 );
+  bb.clear();
+  bb.put_f64(66666.6666).unwrap().clear();
+  assert!( bb.get_f64().unwrap() == 66666.6666 );
+}
+
+#[test]
+fn test_vec() { 
+  macro_rules! vtest {
+    ( $x:tt, $y:expr ) => (
+      interpolate_idents! { 
+        {
+          let tsize = std::mem::size_of::<$x>();
+          let mut bb = ByteBuffer::allocate(tsize * 3);
+          bb.[put_ $x]($y).unwrap() 
+            .[put_ $x]($y).unwrap()
+            .[put_ $x]($y).unwrap()
+            .clear();
+          let mut v = bb.[as_ $x _vec]().unwrap();
+          let mut it = v.iter_mut();
+          assert!( *it.next().unwrap() == $y );
+          assert!( *it.next().unwrap() == $y );
+          assert!( *it.next().unwrap() == $y );
+        }
+      }
+    ) 
+  }
+
+  vtest!(u16, 15);
+  vtest!(u32, 151);
+  vtest!(u64, 7777777);
+  vtest!(i16, 15);
+  vtest!(i32, 666);
+  vtest!(i64, 9999999);
+  vtest!(f32, 666.666);
+  vtest!(f64, 9999999.999999);
+}
